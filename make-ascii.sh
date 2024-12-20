74a8b7cbdce3116402841f3e2e4a4722d6d776a6
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-20 16:34:00 +0800

    correction

--- make-ascii.sh
+++ make-ascii.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Author: Nguyen Khac Trung Kien
+rm -rf frames-bad-apple
 mkdir -p frames-bad-apple
-ffmpeg -i bad_apple.mp4 -vf fps=30 frames-bad-apple/out%04d.jpg
+ffmpeg -i bad_apple.mp4 -vf fps=2 frames-bad-apple/out%04d.jpg
 
-rm  frame-ascii/*
+rm -rf frames-ascii
 mkdir -p frames-ascii
 for file in $PWD/frames-bad-apple/*;
 do
   filename=$(basename "$file")
   echo $filename
-  ascii-image-converter  $file >  frames-ascii/$filename.txt
+  ascii-image-converter -d 30,16 $file >  frames-ascii/$filename.txt
 done
