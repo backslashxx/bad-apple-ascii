06a803f29bff5fa96a48f7f64a6bfd4ca0be0ac9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-21 20:40:31 +0800

    plain utf8

--- make-ascii.sh
+++ make-ascii.sh
@@ -2,13 +2,13 @@
 # Author: Nguyen Khac Trung Kien
 rm -rf frames-bad-apple
 mkdir -p frames-bad-apple
-ffmpeg -i bad_apple.mp4 -vf fps=2 frames-bad-apple/out%04d.jpg
+ffmpeg -i bad_apple.mp4 -vf fps=4 frames-bad-apple/out%04d.jpg
 
 rm -rf frames-ascii
 mkdir -p frames-ascii
 for file in $PWD/frames-bad-apple/*;
 do
   filename=$(basename "$file")
-  echo $filename
-  ascii-image-converter -d 30,16 $file >  frames-ascii/$filename.txt
+  ascii-image-converter -d 30,16 $file | sed -z 's|\n|\\n|g' >> frames-ascii/frames.txt
+  echo "" >> frames-ascii/frames.txt
 done
