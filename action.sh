4d0ecfb8eb6b21aedf9a88d6f4319c93067a4718
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-21 22:24:51 +0800

    utf8 and read lines
    https://github.com/backslashxx/bad-apple-ascii/pull/1

--- action.sh
+++ action.sh
@@ -6,7 +6,7 @@ MODDIR="${0%/*}"
 # you can even run 10 fps on them but 
 # the time skew becomes greater
 if [ -z $MMRL ] && { [ $KSU = true ] || [ $APATCH = true ]; }; then
-	for i in $(cat $MODDIR/frames_1fps.txt); do echo $i | base64 -d ; sleep 1 ; done
+	while read -r frame; do echo -en "$frame"; sleep 1 ;done < $MODDIR/frames_1fps.txt
 else
-	for i in $(cat $MODDIR/frames_4fps.txt); do echo $i | base64 -d ; sleep 0.25 ; done
+	while read -r frame; do echo -en "$frame"; sleep 0.25 ;done < $MODDIR/frames_4fps.txt
 fi
