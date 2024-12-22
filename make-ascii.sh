8b77e9cb646764c1dbb0404b4ca44de9fe351393
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-22 17:40:10 +0800

    adjust newlines

--- make-ascii.sh
+++ make-ascii.sh
@@ -2,7 +2,7 @@
 # Author: Nguyen Khac Trung Kien
 rm -rf frames-bad-apple
 mkdir -p frames-bad-apple
-ffmpeg -i bad_apple.mp4 -vf fps=4 frames-bad-apple/out%04d.jpg
+ffmpeg -i bad_apple.mp4 -vf fps=30 frames-bad-apple/out%04d.jpg
 
 rm -rf frames-ascii
 mkdir -p frames-ascii
@@ -12,3 +12,4 @@ do
   ascii-image-converter -d 30,16 $file | sed -z 's|\n|\\n|g' >> frames-ascii/frames.txt
   echo "" >> frames-ascii/frames.txt
 done
+sed -zi 's|\n|\n\\n|g' frames-ascii/frames.txt
