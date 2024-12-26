49f75095a8139080161cb7b5847ec2e8112be7b2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-26 12:33:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+somehow old ksu and ap cant even handle 0.5 sleep / 2fps
+
+tiann/KernelSU pr#2307 and mmrl latest will do 30 fps
+
+magisk will do 10 fps on this
+
+[Download](https://raw.githubusercontent.com/backslashxx/bad-apple-ascii/refs/heads/magisk-module/bad_apple.zip)
+
+ascii frames generated via 
+
+https://github.com/trung-kieen/bad-apple-ascii
---
+++ action.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+MODDIR="/data/adb/modules/bad_apple"
+
+# ksu and apatch cant handle much shit
+if [ -z "$MMRL" ] && [ "$APATCH" = "true" ]; then
+        while read -r frame; do echo -en "$frame"; sleep 1 ; done < $MODDIR/frames_1fps.txt
+        exit 0
+fi
+
+# requires https://github.com/bryanyee33/KernelSU/commit/3da19277a2efadcc52e2b48c01329ee2c36712ef
+if [ -z "$MMRL" ] && [ "$KSU" = "true" ]; then
+	# actually has to be 0.033 but lets give it a bit of leeway
+	while read -r frame; do echo -en "$frame"; clear ; sleep 0.03 ; done < $MODDIR/frames_30fps.txt
+	exit 0	
+fi
+
+# use mmrl's feature
+# https://github.com/DerGoogler/MMRL/blob/master/docs/INSTALLER.md#clear-terminal
+if [ "$MMRL" = "true" ]; then
+	if echo $CLASSPATH | grep -q "mmrl.debug" > /dev/null 2>&1; then
+		mmrl_clear() { am broadcast -a com.dergoogler.mmrl.debug.CLEAR_TERMINAL > /dev/null 2>&1; }
+	else
+		mmrl_clear() { am broadcast -a com.dergoogler.mmrl.CLEAR_TERMINAL > /dev/null 2>&1; }
+	fi
+	# actually has to be 0.033 but lets give it a bit of leeway
+	while read -r frame; do 
+		mmrl_clear
+		echo -en "$frame"
+		sleep 0.03
+	done < $MODDIR/frames_30fps.txt
+	exit 0	
+fi
+
+while read -r frame; do echo -en "$frame" ; sleep 0.1 ; done < $MODDIR/frames_10fps.txt
+exit 0
+
+
