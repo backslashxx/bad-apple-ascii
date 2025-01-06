5271b6637056bb3065d1f1e0849ff7511f3168ff
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-07 00:25:00 +0800

    scripts/action: handle MMRL generic clear

--- action.sh
+++ action.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 MODDIR="/data/adb/modules/bad_apple"
 
-# mmrl's custom feature
-# https://github.com/DerGoogler/MMRL/blob/master/docs/INSTALLER.md#clear-terminal
-if [ "$MMRL" = "true" ]; then
-        if echo "$CLASSPATH" | grep -q "mmrl.debug" > /dev/null 2>&1; then
-                clear() { am broadcast -a com.dergoogler.mmrl.debug.CLEAR_TERMINAL > /dev/null 2>&1; }
-        else
-                clear() { am broadcast -a com.dergoogler.mmrl.CLEAR_TERMINAL > /dev/null 2>&1; }
-        fi
-fi
-
 # mmrl and kernelsu 11998 and above can clear action window
-# ksu-next too https://github.com/rifsxd/KernelSU-Next/commit/f935077bb80337f578abaa2fd4f7ec1c08baa3ab
-if [ "$MMRL" = "true" ] || { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -ge 11998 ]; } || { [ "$KSU_NEXT" = "true" ] && [ "$KSU_VER_CODE" -ge 12144 ]; }; then
+if [ "$MMRL" = "true" ] || { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -ge 11998 ]; } || 
+	{ [ "$KSU_NEXT" = "true" ] && [ "$KSU_VER_CODE" -ge 12144 ]; }; then
         while read -r frame; do 
                 clear
                 echo -en "$frame"
