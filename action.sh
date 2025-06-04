011207971cdef87918b7dd9c6c78337ebb5d8218
KOWX712 <leecc0503@gmail.com>
2025-06-04 17:28:41 +0800

    action: revert webui environment check
    revert create customize.sh since webui no longer rely on it
    revert changes to action.sh in 2fda1772efc3d6c2487a795c322b74ed06b108a0

--- action.sh
+++ action.sh
@@ -4,10 +4,9 @@ MODDIR="/data/adb/modules/bad_apple"
 # mmrl and ksu 11998, ksu-next 12144, apatch 11022 and above can clear action window
 if [ "$MMRL" = "true" ] || { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -ge 11998 ]; } || 
 	{ [ "$KSU_NEXT" = "true" ] && [ "$KSU_VER_CODE" -ge 12144 ]; } ||
-	{ [ "$APATCH" = "true" ] && [ "$APATCH_VER_CODE" -ge 11022 ]; } ||
-        [ "$KSU_WEBUI" = "true" ]; then
+	{ [ "$APATCH" = "true" ] && [ "$APATCH_VER_CODE" -ge 11022 ]; }; then
         while read -r frame; do 
-                [ "$KSU_WEBUI" = "true" ] || clear
+                clear
                 echo -en "$frame"
                 usleep 33333
         done < $MODDIR/frames_30fps.txt
